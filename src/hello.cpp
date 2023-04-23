5acd860fbf9f4327f400eac753198901f2c89c81
Max <mk14423@bristol.ac.uk>
2023-04-23 16:12:48 +0100

    implimenting vectors

--- src/hello.cpp
+++ src/hello.cpp
@@ -11,6 +11,7 @@ class Person{
 };
 
 int main(){
+
     std::cout << "Hello, World!" << std::endl;
     Person john = Person(20, "John");
     // john.age = 21;
@@ -19,4 +20,20 @@ int main(){
     // const int image_width = 256;
     // const int image_height = 256;
     std::cout << "Hello!" << std::endl;
+
+    int num_elements = 0;
+    std::vector<int> vec = {1, 2, 3};
+    num_elements = std::size(vec);
+    std::cout << num_elements << std::endl;
+    for (int i = 0; i < num_elements; i++){
+        std::cout << vec[i] << " ";
+    }
+    std::cout << std::endl;
+
+    // or equivalently we could do...
+    std::vector<int>::iterator iter;
+    for (iter = vec.begin(); iter != vec.end(); iter++){
+        std::cout << *iter << " ";
+    }
+    // std::cout << vec << std::endl;
 }
